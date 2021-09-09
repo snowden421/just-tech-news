c10aa04a2f46a574de7c5128689105712acacdb5
Snow <84701509+snowden421@users.noreply.github.com>
2021-09-08 18:04:49 -0700

    added comment

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,8 @@ const userRoutes = require('./user-routes.js');
 
 router.use('/users', userRoutes);
 
+const commentRoutes = require('./comment-routes');
+
+router.use('/comments', commentRoutes);
+
 module.exports = router;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,31 +1,34 @@
 const router = require("express").Router();
-const { User, Post, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    // update the `.findAll()` method's attributes to look like this
+    order: [['created_at', 'DESC']],
     attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
+        attributes: ['username']
+      }
+    ]
+   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
       console.log(err);
