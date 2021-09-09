8166bf86fae4e9ccb050784fbfbd15d989ea8679
Snow <84701509+snowden421@users.noreply.github.com>
2021-09-08 17:08:49 -0700

    added password to login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,34 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+
+  // Query operation
+
+})
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -71,6 +99,18 @@ router.put("/:id", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.put('/:id', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  });
 });
 
 // DELETE /api/users/1
