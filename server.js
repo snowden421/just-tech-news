c96d4d4ca06d7800ff1de8ba13f617edcac0501c
Snow <84701509+snowden421@users.noreply.github.com>
2021-09-13 16:28:37 -0700

    fixed database error

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
