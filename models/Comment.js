28075bea7b89eaafb0b8c15f226688a7a0bac44d
Snow <84701509+snowden421@users.noreply.github.com>
2021-09-13 18:04:29 -0700

    added login

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
@@ -7,31 +7,37 @@ Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
-        model: "user",
-        key: "id",
-      },
+        model: 'user',
+        key: 'id'
+      }
     },
     post_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
-        model: "post",
-        key: "id",
-      },
-    },
+        model: 'post',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "comment",
+    modelName: 'comment'
   }
 );
 
